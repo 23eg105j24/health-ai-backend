7d20173ee5430d327a8e1c0985a9d7f4e2f6084c
23eg105j24 <23eg105j24@anurag.edu.in>
2026-04-03 12:11:47 +0530

    update component scan

--- src/main/java/com/example/demo/HealthAiBackendApplication.java
+++ src/main/java/com/example/demo/HealthAiBackendApplication.java
@@ -3,10 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
-@ComponentScan(basePackages = {"com.healthai"})
 public class HealthAiBackendApplication {
 
     public static void main(String[] args) {
